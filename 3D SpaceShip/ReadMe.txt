1d719fc32e633ee5cdd085c1ad8598444738d902
elliottThorley <59701806+elliottThorley@users.noreply.github.com>
2021-12-19 21:31:39 -0500

    Space Ship Added
    Added Spaceship demo and readme

---
+++ 3D SpaceShip/ReadMe.txt
@@ -0,0 +1,11 @@
+Elliott Thorley 12/19/21
+
+----------------DESCRIPTION----------------
+The point of this project is to create an arcade space ship controller.
+If expanded upon, adding a shooting functionality to break astroids would be cool.
+
+----------------CONTROLS----------------
+Q+E: air roll left and right
+A+D: turn left and right
+W+S: tilt up and down
+Space+Ctrl: forward and reverse
---
+++ 3D SpaceShip/Space Ship/Assets/Scripts/VehicalSpace.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicalSpace : MonoBehaviour
+{
+    public float torque = 550;
+    public float thrust = 10000f;
+    private float glide;
+    private Rigidbody rb;
+    public bool throttle;
+    public bool reverse;
+    public int lastUsed;
+    private bool landing = false;
+    private bool reached = false;
+    private bool reached1 = false;
+
+    void Start()
+    {
+        glide = 0f;
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update() {
+        if (Input.GetKeyDown("g")){landing = true;}
+    }
+    void FixedUpdate()
+    {
+        if (landing == true)
+        {
+            if (transform.eulerAngles.z > -30 && transform.eulerAngles.z < 40 || reached1 == true)
+            {
+                reached1 = true;
+                if (transform.eulerAngles.x > 60 && transform.eulerAngles.x < 90)
+                {
+                    reached = true;
+                }
+                else if (reached == false)
+                {
+                    rb.AddRelativeTorque(Vector3.right * torque * -3);
+                }
+                if (reached == true)
+                {
+                    rb.AddRelativeForce(Vector3.forward * thrust * 3);
+                    glide = thrust;
+                    lastUsed = 0;
+                }
+            }
+            else
+            {
+                rb.AddRelativeTorque(Vector3.back * torque * 2f);
+            }
+        }
+        else {
+            float roll = Input.GetAxisRaw("Horizontal");
+            float pitch = Input.GetAxis("Vertical");
+            throttle = Input.GetKey("space");
+            reverse = Input.GetKey(KeyCode.LeftControl);
+            bool left = Input.GetKey("a");
+            bool right = Input.GetKey("d");
+
+            rb.AddRelativeTorque(Vector3.back * torque * roll);
+            rb.AddRelativeTorque(Vector3.right * torque * pitch);
+
+           
+
+            if (left == true) {
+                rb.AddRelativeTorque(Vector3.down * torque);
+            }
+            if (right == true)
+            {
+                rb.AddRelativeTorque(Vector3.up * torque);
+            }
+
+            if (throttle == true && reverse == false)
+            {
+                rb.AddRelativeForce(Vector3.forward * thrust);
+                glide = thrust;
+                lastUsed = 0;
+            }
+            else if (throttle == false && reverse == true)
+            {
+                rb.AddRelativeForce(Vector3.back * thrust);
+                glide = thrust;
+                lastUsed = 1;
+            }
+            else
+            {
+                if (lastUsed == 0)
+                    rb.AddRelativeForce(Vector3.forward * glide);
+                if (lastUsed == 1)
+                    rb.AddRelativeForce(Vector3.back * glide);
+                glide *= 0.9f;
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
